bdf93ad538efede366e7a84c30c075f20aa7ca6f
amj9396 <amj9396@rit.edu>
2020-03-01 09:44:47 -0500

    added images

--- ChatBot/Src/Chat/ChatSession.java
+++ ChatBot/Src/Chat/ChatSession.java
@@ -1,7 +1,15 @@
 package Chat;
 
+import java.util.Scanner;
+
 public class ChatSession {
     public ChatSession(){
+
+    }
+
+    public void readCmd(){
+        Scanner in = new Scanner(System.in);
+
 
     }
 
