8d2adee979349959bd07e9e7cdcf91fc39645ab1
amj9396 <amj9396@rit.edu>
2020-03-01 10:17:49 -0500

    please work

--- ChatBot/Src/Chat/MainChatFile.java
+++ ChatBot/Src/Chat/MainChatFile.java
@@ -1,6 +1,13 @@
 package Chat;
 
+import java.util.Random;
+
 public class MainChatFile {
+
+    public void empowerment(){
+        Random randint = new Random;
+        return empower[randint]
+    }
     public static void main(System[] args){
         ChatSession session = new ChatSession();
     }
